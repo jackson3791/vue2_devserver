43cf56059f3e66b85785c8070444d0fdd39476ab
jackson <1032399389@qq.com>
2022-05-13 23:14:39 +0800

    试验成功

--- express/index.js
+++ express/index.js
@@ -1,11 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-app.get('/proxy', function (req, res) {
-    res.send('跨域请求成功')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.get("/proxy", function (req, res) {
+  res.send("跨域请求成功");
+});
+app.get("/testtwo", function (req, res) {
+  res.send("跨域请求成功22");
+});
 
-app.listen(3000)
+app.listen(3000);
